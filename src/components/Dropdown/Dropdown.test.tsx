415c31956f23a00b28ab1ffb9e80f237de0c1b92
Preet191200 <pkachhia@rrc.ca>
2026-03-21 19:32:04 -0500

    fix: remove unused userEvent import in Dropdown test

--- src/components/Dropdown/Dropdown.test.tsx
+++ src/components/Dropdown/Dropdown.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Dropdown from "./Dropdown";
 
 test("Dropdown is visible", () => {
